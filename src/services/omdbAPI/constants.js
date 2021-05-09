ef58d97e0bf378cf7ec54bc57df466a1be93b848
Thisisme-Andrew <andy.tangg4@gmail.com>
2021-05-09 10:02:38 -0600

    hghgkhgkj

--- src/services/omdbAPI/constants.js
+++ src/services/omdbAPI/constants.js
@@ -1,4 +1,4 @@
-export const ROOT_URL = 'http://www.omdbapi.com/';
+export const ROOT_URL = 'https://www.omdbapi.com/';
 export const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 export const TYPE_MOVIE = 'movie';
 export const TYPE_SERIES = 'series';
